13eb6160270d71e53cabb74e4e599f172818221a
Shubham Kumar <Swapnil Kant>
2024-05-17 21:12:13 +0530

    SH-GO01-jwt-authentication-user-models

---
+++ models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Username string `json:"username" gorm:"unique"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
+}
